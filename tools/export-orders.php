9998ff39a0cad8b09b1609f7748d9412971cb0da
barakchamo <barak@ometria.com>
2014-04-25 12:20:36 +0100

    Exporter now saves records to a json file

--- tools/export-orders.php
+++ tools/export-orders.php
@@ -2,7 +2,9 @@
 /**
  * Usage: php export-orders.php --timeFrom=date --timeTo=date --limit=100 --output='myfile' --format='json'
  */
+echo "Ometria API Client \nPress Ctrl+C to abort.\n\n";
 
+// Load the Ometria API Client class.
 require('../lib/OmetriaAPI/Client.php');
 
 // Load the configuration file (this file must be created following config-example.php).
@@ -17,10 +19,13 @@ $offset       = 0;
 $page_size    = 250;
 $page_count   = 0;
 $data         = array();
+$parsed_args  = array();
+$resource     = 'transactions';
 $res;
+$export;
+$file;
 
-// how to get commandline params:
-$parsed_args = array();
+// Parse CLI arguements
 for($i=1; $i<count($argv);$i++){
 	$raw = $argv[$i];
 
@@ -29,69 +34,79 @@ for($i=1; $i<count($argv);$i++){
 		$raw = substr($raw,2);
 		$key = strtok($raw, '=');
 		$value = strtok('=');
+
+		// Store arguemnt key:value in $parsed_args array.
 		$parsed_args[$key] = $value;
 
+		// If the arguement is a valid query parameter, add it to the query string.
 		if (in_array($key, $query_params)) {
 			$query_string[$key] = $value;
 		}
 	}
 }
 
-//$offset = @$parsed_args['offset'] ?: 0;
+echo "Querying for ", $resource, "...\n\n";
+
+// If an offset is specified, add it to the query string, otherwise start from record 0.
 $offset = isset($parsed_args['offset']) ? $parsed_args['offset'] : 0;
-// $limit  = $parsed_args['limit'] ?: 100000000;
 
-// algorithm will be:
-// 1) Get first <limit> results
-// 2) If number of results < <limit> break
-// 3) Repeat
-// Require the Ometria Client library.
+// Set the limit of the query to the maximum page size (specified to assure operation of the client).
+$query_string['limit'] = $page_size;
 
-function get_data($offset, $limit) {
-	global $res, $client, $data, $page_count, $query_string;
-
-	$query_string['limit'] = $limit;
-	$query_string['offset'] = $page_count  * $limit;
-	print_r($query_string);
-	$res = $client->get('transactions', $query_string);
-
-	if (is_array($res)) {
-		$data = array_merge($data, $res);
-		echo(count($res)),"\n";
-		echo(count($data)),"\n";
-		//print_r($data);
-	}
-
-	$page_count++;
-	echo 'page ',$page_count,"\n";
-
-	if (count($res) == $limit) {
-		sleep(1);
-		get_data($offset, $limit);
-	}
-}
-
-get_data($offset, $page_size);
-
-echo "\n And the total is....... \n";
-echo count($data);
-echo "\n";
-
-
-$page_size  = 250;
-$offset = 0;
-
+// Query the API for resource records
 while(true){
-	$qs = array();
-	$qs['limit'] = $page_size;
-	$qs['offset'] = $offset;
+	// re-set the offset to retrieve the next batch of records by page size.
+	$query_string['offset'] = $offset;
+
+	// Submit a get request to the Ometria API for the required resource and query parameters.
 	$res = $client->get('transactions', $query_string);
 
-	// append
+	// Check if the API response returned a valid array.
+	if (is_array($res)) {
+		echo "\r* Retrieved ", $resource, " records ", ($offset - count($data)), " to ", ($offset + count($res)), "...";
 
+		// Merge the responce into the previously retrieved data set.
+		$data = array_merge($data, $res);
+	}
+
+	// Increment the offset by the page size retrieved.
+	$offset += $page_size;
+
+	// Check if the response array's length matches the page size.
 	if (count($res)<$page_size) {
+
+		// Break out of the while loop if the responce is smaller than the page size (means that the end of the data set was reached).
 		break;
 	}
-
-	$offset += $page_size;
 }
+
+echo "\n\n Completed data fetch. \n Retrieved a total of ", count($data), " ", $resource," records.\n";
+
+// Convert the result data set to JSON.
+$export = json_encode($data, JSON_PRETTY_PRINT) or die("Could not create export file.");
+
+// Create a file to export the data.
+$file = "Ometria_API_".$resource."_export";
+$extension = ".json";
+$i = 1;
+$file_name = $file;
+// Check if file exists and append a serializer
+while(file_exists($file_name.$extension))
+{
+    $file_name = $file."_".$i;
+    $i++;
+}
+
+// Set file name to serialized name.
+$file = $file_name.$extension;
+
+// Create and pen file with writing permission.
+$handle = fopen($file, "wb");
+
+// Write JSON string to $file.
+fwrite($handle, $export);
+
+// Close the file after writing.
+fclose($handle);
+
+echo "\nData saved to file - ",$file,"\n";
